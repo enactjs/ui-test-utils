dffa1d1aa88e398af9594815cb8d41d14b74fdac
Roy Sutton <roy.sutton@lge.com>
2017-05-01 15:03:02 -0400

    Add test command
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- test/test-specs.js
+++ test/test-specs.js
@@ -3,11 +3,6 @@ let Page = require('./Page');
 describe('Spotlight', function () {
 
 	it('should focus and unfocus the button', function () {
-		browser.url('http://webdriver.io').elements('div').getAttribute('id')
-		//Page.open('http://webdriver.io/');
-		//Page.button.moveToObject();
-		//$('#docsearch').moveToObject(0,0);
-		//		browser.url('localhost:4567');
 		Page.open('localhost:4567');
 		Page.button.moveToObject().buttonDown();
 		expect(Page.button.hasFocus()).to.be.true();
