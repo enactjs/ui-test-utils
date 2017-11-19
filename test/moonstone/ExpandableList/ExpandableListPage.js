c801634b05e8711ee1a2d654b88a7e888ced7c1c
Roy Sutton <roy.sutton@lge.com>
2017-11-19 03:46:25 -0500

    Add ExpandableList app and 5-way tests
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

---
+++ test/moonstone/ExpandableList/ExpandableListPage.js
@@ -0,0 +1,44 @@
+'use strict';
+const Page = require('../../Page.js');
+
+class ExpandableInterface {
+	constructor (id) {
+		this.id = id;
+	}
+
+	focus () {
+		return browser.selectorExecute(`#${this.id}>div`, (els) => els && !els[0].focus());
+	}
+
+	get title () { return browser.element(`#${this.id}>div`); }
+	get titleText () { return browser.element(`#${this.id}>div .Marquee__text`).getText(); }
+	get chevron () { return browser.element(`#${this.id}>div .Icon__icon`).getText(); }
+	get value () { return browser.element(`#${this.id}>div>div:nth-of-type(2)`); }
+	get valueText () { return browser.element(`#${this.id}>div>div:nth-of-type(2)`).getText(); }
+	get isOpen () { return browser.isExisting(`#${this.id} .Transition__shown`); }
+	get item1 () { return browser.element(`#${this.id} [role="checkbox"]`); }
+	get item2 () { return browser.element(`#${this.id} [role="checkbox"]:nth-of-type(2)`); }
+	get item3 () { return browser.element(`#${this.id} [role="checkbox"]:nth-of-type(3)`); }
+}
+
+class SpotlightMultiplePage extends Page {
+	constructor () {
+		super();
+		this.title = 'ExpandableList Test';
+		this.expandable1 = new ExpandableInterface('expandable1');
+		this.expandable2 = new ExpandableInterface('expandable2');
+		this.expandable3 = new ExpandableInterface('expandable3');
+		this.expandable4 = new ExpandableInterface('expandable4');
+		this.expandable5 = new ExpandableInterface('expandable5');
+		this.expandable6 = new ExpandableInterface('expandable6');
+	}
+
+	open (urlExtra) {
+		super.open('ExpandableList-View', urlExtra);
+	}
+
+}
+
+module.exports = new SpotlightMultiplePage();
+
+
