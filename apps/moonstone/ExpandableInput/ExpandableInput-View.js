8fd63c2de10194280abf0d73083b10c9109d7b9e
srirama.singeri <srirama.singeri@lge.com>
2017-12-21 19:09:42 +0530

    Create UI tests for ExpandableInput

---
+++ apps/moonstone/ExpandableInput/ExpandableInput-View.js
@@ -0,0 +1,52 @@
+import ExpandableInput from '@enact/moonstone/ExpandableInput';
+import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+
+// ExpandableInput options:
+// disabled, noneText, title, placeholder, iconBefore, iconAfter, type
+// defaultOpen, defaultValue
+
+const app = (props) => <div {...props}>
+	<div>
+		<ExpandableInput
+			id="expandable1"
+			title="ExpandableInput Default"
+			noneText="No Input Text"
+		/>
+		<ExpandableInput
+			id="expandable2"
+			title="ExpandableInput Default Value"
+			noneText="No Input Text"
+			defaultValue="Default Value"
+		/>
+		<ExpandableInput
+			id="expandable3"
+			title="ExpandableInput Default Open"
+			noneText="No Input Text"
+			defaultOpen
+		/>
+		<ExpandableInput
+			id="expandable4"
+			title="ExpandableInput Password"
+			noneText="No Input Text"
+			type ="password"
+			defaultValue="Password"
+		/>
+		<ExpandableInput
+			id="expandable5"
+			title="ExpandableInput Disabled"
+			noneText="No Input Text"
+			disabled
+		/>
+	</div>
+</div>;
+
+export default MoonstoneDecorator(app);
+
+
---
+++ test/moonstone/ExpandableInput/ExpandableInputPage.js
@@ -0,0 +1,52 @@
+'use strict';
+const Page = require('../../Page.js');
+const {element, getComponent, getSubComponent, getText} = require('../../utils.js');
+
+const getIcon = getComponent('Icon');
+const getInput = getComponent('Input');
+const getLabeledItem = getComponent('LabeledItem');
+const getLabeledItemTitle = getSubComponent('LabeledItem', 'title');
+const getLabeledItemValue = getSubComponent('LabeledItem', 'label');
+
+class ExpandableInterface {
+	constructor (id) {
+		this.id = id;
+	}
+
+	focus () {
+		return browser.selectorExecute(`#${this.id}>div`, (els) => els && !els[0].focus());
+	}
+
+	get self () { return element(`#${this.id}`, browser); }
+	get input () { return getInput(this.self); }
+	get chevron () { return getText(getIcon(this.self)); }
+	get title () { return getLabeledItem(this.self); }
+	get titleText () { return getText(getLabeledItemTitle(this.self)); }
+	get label () { return getLabeledItemValue(this.self); }
+	get labelText () { return getText(this.label); }
+	get isLabelExists () { return this.self.isExisting('.LabeledItem__label'); }
+	get isOpen () { return this.self.isExisting('.Transition__shown'); }
+
+}
+
+class SpotlightMultiplePage extends Page {
+	constructor () {
+		super();
+		this.title = 'ExpandableInput Test';
+		this.components = {};
+		this.components.default = new ExpandableInterface('expandable1');
+		this.components.defaultValue = new ExpandableInterface('expandable2');
+		this.components.defaultOpen = new ExpandableInterface('expandable3');
+		this.components.password = new ExpandableInterface('expandable4');
+		this.components.disabled = new ExpandableInterface('expandable5');
+	}
+
+	open (urlExtra) {
+		super.open('ExpandableInput-View', urlExtra);
+	}
+
+}
+
+module.exports = new SpotlightMultiplePage();
+
+
