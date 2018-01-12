a030a3247d8d1f85888347734bebbe60fa5545bd
Madala Cholan Satyanarayana <cholanmadala1510@gmail.com>
2018-01-13 05:29:22 +0530

    Create UI tests for ToggleButton (#21)
    * Create UI tests for ToggleButton
    
    * Made changes as per review comments
    
    * Added configuration labels to the individual components
    
    * Added two new cases and review changes
    
    * Make tags self closing
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

---
+++ test/moonstone/ToggleButton/ToggleButtonPage.js
@@ -0,0 +1,43 @@
+'use strict';
+const Page = require('../../Page.js');
+
+class ToggleButtonInterface {
+	constructor (id) {
+		this.id = id;
+	}
+
+	focus () {
+		return browser.selectorExecute(`#${this.id}`, (els) => els && !els[0].focus());
+	}
+
+	get item () { return browser.element(`#${this.id}`); }
+	get valueText () { return browser.element(`#${this.id} > div .Marquee__text`).getText(); }
+	get isSelected () { return browser.isExisting(`#${this.id}.ToggleButton__selected.Button__selected`); }
+	get isSmall () { return browser.isExisting(`#${this.id}.ToggleButton__small`); }
+}
+
+class ToggleButtonPage extends Page {
+	constructor () {
+		super();
+		this.title = 'ToggleButton Test';
+		const toggleDefault = new ToggleButtonInterface('toggleButton1');
+		const toggleWithLabels = new ToggleButtonInterface('toggleButton2');
+		const toggleWithOnlyOnLabel = new ToggleButtonInterface('toggleButton3');
+		const toggleWithOnlyOffLabel = new ToggleButtonInterface('toggleButton4');
+		const toggleDefaultSelected = new ToggleButtonInterface('toggleButton5');
+		const toggleDisabled = new ToggleButtonInterface('toggleButton6');
+		const toggleSmall = new ToggleButtonInterface('toggleButton7');
+		const toggleCasePreserve = new ToggleButtonInterface('toggleButton8');
+		const toggleCaseSentence = new ToggleButtonInterface('toggleButton9');
+		const toggleCaseWord = new ToggleButtonInterface('toggleButton10');
+		const toggleCaseUpper = new ToggleButtonInterface('toggleButton11');
+
+		this.components = {toggleDefault, toggleWithLabels, toggleWithOnlyOnLabel, toggleWithOnlyOffLabel, toggleDefaultSelected, toggleDisabled, toggleSmall, toggleCasePreserve, toggleCaseSentence, toggleCaseWord, toggleCaseUpper}
+	}
+
+	open (urlExtra) {
+		super.open('ToggleButton-View', urlExtra);
+	}
+}
+
+module.exports = new ToggleButtonPage();
