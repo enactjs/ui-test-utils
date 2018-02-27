7e7e252be7212f54377553b45f5439ecd5bd49d6
Jason Robitaille <robitaille88@gmail.com>
2018-02-26 16:56:27 -0800

    PLAT-50786: Refactored for current enact-cli build standards and building views in parallel to save time.

---
+++ index.js
@@ -0,0 +1,18 @@
+import '@babel/polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import App from 'UI_TEST_APP_ENTRY';
+	
+const url = new URL(window.location.href);
+const locale = url.searchParams.get('locale');
+
+const appElement = (<App locale={locale}/>);
+
+if (typeof window !== 'undefined') {
+	render(
+		appElement,
+		document.getElementById('root')
+	);
+}
+
+export default appElement;
