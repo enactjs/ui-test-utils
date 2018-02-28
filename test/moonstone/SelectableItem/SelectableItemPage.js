99651e3f01709b632990f76b619d6f19241eb6f8
Roy Sutton <roy.sutton@lge.com>
2018-02-27 19:54:12 -0500

    Fix SelectableItem for spotlight
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- test/moonstone/SelectableItem/SelectableItemPage.js
+++ test/moonstone/SelectableItem/SelectableItemPage.js
@@ -10,7 +10,7 @@ class SelectableItemInterface {
 		return browser.selectorExecute(`#${this.id}`, (els) => els && !els[0].focus());
 	}
 
-	get item () { return browser.element(`#${this.id}`); }
+	get self () { return browser.element(`#${this.id}`); }
 	get valueText () { return browser.element(`#${this.id} > div .Marquee__text`).getText(); }
 	get isSelected () { return browser.isExisting(`#${this.id} .SelectableItem__selected`); }
 	get isToggled () { return browser.isExisting(`#${this.id} .ToggleItem__selected`); }
