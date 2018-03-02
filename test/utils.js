75341609b285713840f5fbfc79f52d306d0cc92c
Dave Freeman <dave.freeman@lge.com>
2018-03-02 16:03:09 -0600

    update utils for new component className rules

--- test/utils.js
+++ test/utils.js
@@ -16,21 +16,22 @@ const getText = (el) => el.getText();
 
 // Given n CSS class names, return a selector with them joined per our CSS Module plugin rules
 // String... => String
-const cssModuleSelector = (...args) => `.${args.join('__')}`;
+const cssModuleSelector = (...args) => `.${args.join('_')}`;
 
-// Given a component name, generates a CSS selector in the form: ComponentName__componentname
-// String => String
-const componentSelector = converge(cssModuleSelector, [identity, toLower]);
+const toEnactPath = (lib, component) => `enact_${lib}_${component}`;
 
 // Given a component name and starting element, returns the first matching descendant Element
 // String => Element => Element
-const getComponent = useWith(element, [componentSelector, identity]);
+const getComponent = curry((lib, component, el) => element(
+	cssModuleSelector(toEnactPath(lib, component), component, toLower(component)),
+	el
+));
 
 // Given a component name, child name, and starting element, returns the first matching descendant Element
 // String => String => Element => Element
-const getSubComponent = curry((name, child, el) => element(
-	cssModuleSelector(name, child),
-	getComponent(name, el)
+const getSubComponent = curry((lib, component, child, el) => element(
+	cssModuleSelector(toEnactPath(lib, component), component, child),
+	getComponent(lib, component, el)
 ));
 
 module.exports = {
