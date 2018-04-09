de9f519e4bee05208f0b88898fddcc291a9207f5
Roy Sutton <roy.sutton@lge.com>
2018-04-09 15:20:30 -0400

    Removed ToggleItem
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- apps/moonstone/ActivityPanels/ActivityPanels-View.js
+++ apps/moonstone/ActivityPanels/ActivityPanels-View.js
@@ -37,43 +37,43 @@ class App extends React.Component {
 				<MainPanel title="First" onClick={this.handleClick} />
 				<ItemPanel title="Second" onClick={this.handleClick} />
 				<ButtonPanel title="Third" onClick={this.handleClick} />
-                <MainPanel title="None" autoFocus="none" onClick={this.handleClick} />
-                <ItemPanel title="Default Element" autoFocus="default-element" />
+				<MainPanel title="None" autoFocus="none" onClick={this.handleClick} />
+				<ItemPanel title="Default Element" autoFocus="default-element" />
 			</ActivityPanels>
 		);
 	}
 }
 
 const ButtonPanel = ({title, onClick, ...rest}) => (
-    <Panel {...rest}>
-        <Header title={title}>
-            <Button id="button3" onClick={onClick}>Button 3</Button>
-            <Button id="button4" onClick={onClick}>Button 4</Button>
-        </Header>
-    </Panel>
+	<Panel {...rest}>
+		<Header title={title}>
+			<Button id="button3" onClick={onClick}>Button 3</Button>
+			<Button id="button4" onClick={onClick}>Button 4</Button>
+		</Header>
+	</Panel>
 )
 
 const ItemPanel = ({title, onClick, ...rest}) => (
-    <Panel {...rest}>
-        <Header title={title} />
-        <Item id="item5" onClick={onClick}>Item 5</Item>
-        <Item id="item6" onClick={onClick}>Item 6</Item>
-        <Item id="item7" onClick={onClick}>Item 7</Item>
-        <Item id="item8" onClick={onClick}>Item 8</Item>
-    </Panel>
+	<Panel {...rest}>
+		<Header title={title} />
+		<Item id="item5" onClick={onClick}>Item 5</Item>
+		<Item id="item6" onClick={onClick}>Item 6</Item>
+		<Item id="item7" onClick={onClick}>Item 7</Item>
+		<Item id="item8" onClick={onClick}>Item 8</Item>
+	</Panel>
 );
 
 const MainPanel = ({title, onClick, ...rest}) => (
-    <Panel {...rest}>
-        <Header title={title}>
-            <Button id="button1" onClick={onClick}>Button 1</Button>
-            <Button id="button2" onClick={onClick}>Button 2</Button>
-        </Header>
-        <Item id="item1" onClick={onClick}>Item 1</Item>
-        <Item id="item2" onClick={onClick}>Item 2</Item>
-        <Item id="item3" onClick={onClick}>Item 3</Item>
-        <Item id="item4" onClick={onClick}>Item 4</Item>
-    </Panel>
+	<Panel {...rest}>
+		<Header title={title}>
+			<Button id="button1" onClick={onClick}>Button 1</Button>
+			<Button id="button2" onClick={onClick}>Button 2</Button>
+		</Header>
+		<Item id="item1" onClick={onClick}>Item 1</Item>
+		<Item id="item2" onClick={onClick}>Item 2</Item>
+		<Item id="item3" onClick={onClick}>Item 3</Item>
+		<Item id="item4" onClick={onClick}>Item 4</Item>
+	</Panel>
 )
 
 export default MoonstoneDecorator(App);
