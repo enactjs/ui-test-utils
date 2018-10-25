175164661addf4a0539c9332cdc480be17ffbb51
Roy Sutton <roy.sutton@lge.com>
2018-10-25 18:10:59 -0400

    Add travis support
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- wdio.conf.js
+++ wdio.conf.js
@@ -44,7 +44,7 @@ exports.config = {
 		// maxInstances can get overwritten per capability. So if you have an in-house Selenium
 		// grid with only 5 firefox instances available you can make sure that not more than
 		// 5 instances get started at a time.
-		maxInstances: 2,
+		maxInstances: 'TRAVIS' in process.env ? 1 : 2,
 		//
 		browserName: 'chrome'
 	}],
@@ -249,4 +249,3 @@ exports.config = {
 	// onComplete: function(exitCode) {
 	// }
 }
-
