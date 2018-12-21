60d44bed9c4ae473176127cb01c92ef9bca0ad5e
Ryan Duffy <ryan@tiqtech.com>
2018-12-20 18:08:21 -0600

    ENYO-5733: Remove spotlightRestrict none tests from Popup (#44)
    * remove spotlightRestrict none tests
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * remove entry from description
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * add placeholder button
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- apps/moonstone/Popup/Popup-View.js
+++ apps/moonstone/Popup/Popup-View.js
@@ -25,7 +25,6 @@ class app extends Component {
 		open3: false,
 		open4: false,
 		open5: false,
-		open6: false,
 		open7: false,
 		open8: false,
 		open9: false
@@ -38,7 +37,7 @@ class app extends Component {
 			<div id="popupMain" {...this.props}>
 				<p>
 					UI testing for Popup Component 1. AutoDismiss 2. noAutoDismiss 3. no Components 4. noAnimation
-					5. without Close Button 6. spotlightRestrict none 7. spotlightRestrict self-first 8. scrimType transparent 9. scrimType none
+					5. without Close Button 7. spotlightRestrict self-first 8. scrimType transparent 9. scrimType none
 				</p>
 				<div style={style.main}>
 					<Button id="buttonPopup1" onClick={() => this.clickHandler({open1: true})}>AutoDismiss</Button>
@@ -46,7 +45,7 @@ class app extends Component {
 					<Button id="buttonPopup3" onClick={() => this.clickHandler({open3: true})}>no Component</Button>
 					<Button id="buttonPopup4" onClick={() => this.clickHandler({open4: true})}>noAnimation</Button>
 					<Button id="buttonPopup5" onClick={() => this.clickHandler({open5: true})}>noCloseButton</Button>
-					<Button id="buttonPopup6" onClick={() => this.clickHandler({open6: true})}>spotlightRestrict none</Button>
+					<Button>---</Button> {/* placeholder to avoid reworking all the tests because a button was removed */}
 					<Button id="buttonPopup7" onClick={() => this.clickHandler({open7: true})}>spotlightRestrict self-first</Button>
 					<Button id="buttonPopup8" onClick={() => this.clickHandler({open8: true})}>scrimType transparent</Button>
 					<Button id="buttonPopup9" onClick={() => this.clickHandler({open9: true})}>scrimType none</Button>
@@ -124,22 +123,6 @@ class app extends Component {
 					<Container>
 						<Button id="buttonOK" onClick={() => this.clickHandler({open5: false})}>OK</Button>
 						<Button id="buttonCancel" onClick={() => this.clickHandler({open5: false})}>Cancel</Button>
-					</Container>
-				</Popup>
-				<Popup
-					id="popup6"
-					open={this.state.open6}
-					noAnimation={false}
-					noAutoDismiss={false}
-					showCloseButton
-					spotlightRestrict="none"
-					onClose={() => this.clickHandler({open6: false})}
-				>
-					<div>Popup spotlightRestrict is none</div>
-					<br />
-					<Container>
-						<Button id="buttonOK" onClick={() => this.clickHandler({open6: false})}>OK</Button>
-						<Button id="buttonCancel" onClick={() => this.clickHandler({open6: false})}>Cancel</Button>
 					</Container>
 				</Popup>
 				<Popup
