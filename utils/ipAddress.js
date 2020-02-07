1eb026bc9d18a303d80c3ae07ed7b20bc2840263
Roy Sutton <roy.sutton@lge.com>
2020-02-07 13:47:16 -0500

    Add ipAddress utility
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

---
+++ utils/ipAddress.js
@@ -0,0 +1,20 @@
+const os = require('os');
+
+function ipAddress () {
+	const ifaces = os.networkInterfaces();
+	let address = 'localhost';
+
+	Object.keys(ifaces).forEach(function (ifname) {
+		let iface = ifaces[ifname][0];
+
+		if (!iface || 'IPv4' !== iface.family || iface.internal !== false) {
+			// skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+			return;
+		}
+
+		address = iface.address;
+	});
+	return address;
+}
+
+module.exports = ipAddress;
