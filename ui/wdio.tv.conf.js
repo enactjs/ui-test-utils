ac39c0491c7d8e58056b24d913584906aa2f521c
Jeonghee Ahn <jeonghee27.ahn@lge.com>
2020-06-19 08:05:52 +0900

    PLAT-110982: Support ui-test on TV (#90)
    * PLAT-110982: Support ui-test on TV
    Enact-DCO-1.0-Signed-off-by: Jeonghee Ahn <jeonghee27.ahn@lge.com>
    
    * Remove unneeded line
    Enact-DCO-1.0-Signed-off-by: Jeonghee Ahn <jeonghee27.ahn@lge.com>

--- ui/wdio.tv.conf.js
+++ ui/wdio.tv.conf.js
@@ -1,9 +1,21 @@
+const ipAddress = require('../utils/ipAddress.js');
 const {config} = require('./wdio.conf.js');
 
 exports.config = Object.assign(
 	{},
 	config,
 	{
+		capabilities: [{
+			maxInstances: 1,
+
+			browserName: 'chrome',
+			'goog:chromeOptions': {
+				debuggerAddress: `${process.env.TV_IP}:9998`
+			}
+		}],
+
+		baseUrl: `http://${ipAddress()}:4567`,
+
 		before: function () {
 			if (config.before) config.before();
 			browser._options = {remote: true};
