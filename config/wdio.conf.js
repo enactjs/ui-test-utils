4c111ab52242ddde7d4f561990e9ecef86f47bb0
Juwon Jeong <juwon.jeong@lge.com>
2022-06-22 17:35:43 +0900

    Fixed wdio.config.js to skip selenium install
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -169,7 +169,7 @@ module.exports.configure = (options) => {
 			// commands. Instead, they hook themselves up into the test process.
 			services: [
 				['selenium-standalone', {
-					skipSeleniumInstall: offline,
+					skipSeleniumInstall: true,
 					args: {
 						drivers : {
 							chrome : {
