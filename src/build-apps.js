fe88d01cf57c6cc7a02e83398a8cc70ae8bdc0e4
Alex Morariu <alexandru.morariu@lgepartner.com>
2023-07-20 22:52:46 +0300

    Changed the chalk to apply inside functions.

--- src/build-apps.js
+++ src/build-apps.js
@@ -16,85 +16,87 @@ function findViews (base) {
 }
 
 function buildApps (base) {
-	import('chalk').then(({default: chalk}) => {
-		if (process.argv.includes('--skip-build')) return;
-		console.log('Building content:\n');
+	if (process.argv.includes('--skip-build')) return;
+	console.log('Building content:\n');
 
-		return Promise.resolve()
-			.then(() => {
-				if (!process.argv.includes('--skip-enact')) {
-					epack({
-						file: {basename: 'Enact framework bundle', fullPath: 'framework'},
-						opts: [
-							'pack',
-							'--output',
-							path.join('tests', base, 'dist', 'framework'),
-							'--framework',
-							'--externals-polyfill'
-						]
-					});
+	return Promise.resolve()
+		.then(() => {
+			if (!process.argv.includes('--skip-enact')) {
+				epack({
+					file: {basename: 'Enact framework bundle', fullPath: 'framework'},
+					opts: [
+						'pack',
+						'--output',
+						path.join('tests', base, 'dist', 'framework'),
+						'--framework',
+						'--externals-polyfill'
+					]
+				});
+			}
+		})
+		.then(() => {
+			if (!process.argv.includes('--skip-ilib')) {
+				const ilibDist = path.join('tests', base, 'dist', 'framework', 'ilib');
+				if (!fs.existsSync(ilibDist)) {
+					fs.mkdirSync(ilibDist);
 				}
-			})
-			.then(() => {
-				if (!process.argv.includes('--skip-ilib')) {
-					const ilibDist = path.join('tests', base, 'dist', 'framework', 'ilib');
-					if (!fs.exists(ilibDist)) {
-						fs.mkdir(ilibDist);
-					}
-					process.stdout.write('\tiLib locale data... ');
-					return fs.copy(
-						path.join('node_modules', 'ilib', 'locale'),
-						path.join(ilibDist, 'locale')
-					).then(() => {
-						if (process.stdout.isTTY) {
-							clearLine();
+				process.stdout.write('\tiLib locale data... ');
+				return fs.copy(
+					path.join('node_modules', 'ilib', 'locale'),
+					path.join(ilibDist, 'locale')
+				).then(() => {
+					if (process.stdout.isTTY) {
+						clearLine();
+						import('chalk').then(({default: chalk}) => {
 							process.stdout.write(chalk.green('\t✔ ') + 'iLib locale data\n');
-						} else {
-							process.stdout.write('DONE\n');
-						}
-					});
-				}
-			})
-			.then(() => {
-				if (!process.argv.includes('--skip-tests')) {
-					return findViews(base).then(files => (
-						files.forEach(file => (
-							epack({
-								file,
-								opts: [
-									'pack',
-									'--entry',
-									path.join(__dirname, '..', base, 'index.js'),
-									'--output',
-									path.join('tests', base, 'dist', path.basename(file.fullPath, '.js')),
-									'--externals',
-									'tests/' + base + '/dist/framework',
-									'--externals-polyfill'
-								]
-							})
-						))
-					));
-				}
-			})
-			.then(() => {
-				if (base.includes('screenshot')) {
-					const distUtils = path.join('tests', base, 'dist', 'utils'),
-						redistSrc = path.join(__dirname, '..', 'screenshot', 'utils', 'redist');
-
-					if (!fs.exists(distUtils)) {
-						fs.mkdir(distUtils);
+						});
+					} else {
+						process.stdout.write('DONE\n');
 					}
+				});
+			}
+		})
+		.then(() => {
+			if (!process.argv.includes('--skip-tests')) {
+				return findViews(base).then(files => (
+					files.forEach(file => (
+						epack({
+							file,
+							opts: [
+								'pack',
+								'--entry',
+								path.join(__dirname, '..', base, 'index.js'),
+								'--output',
+								path.join('tests', base, 'dist', path.basename(file.fullPath, '.js')),
+								'--externals',
+								'tests/' + base + '/dist/framework',
+								'--externals-polyfill'
+							]
+						})
+					))
+				));
+			}
+		})
+		.then(() => {
+			if (base.includes('screenshot')) {
+				const distUtils = path.join('tests', base, 'dist', 'utils'),
+					redistSrc = path.join(__dirname, '..', 'screenshot', 'utils', 'redist');
 
-					return fs.copy(redistSrc, distUtils);
+				if (!fs.existsSync(distUtils)) {
+					fs.mkdirSync(distUtils);
 				}
-			})
-			.catch(err => {
+
+				return fs.copy(redistSrc, distUtils);
+			}
+		})
+		.catch(err => {
+			import('chalk').then(({default: chalk}) => {
 				console.error(chalk.red('Build failed:'));
-				console.error();
-				console.error(err.message);
-				process.exit(1);
 			});
-	});
+			console.error();
+			console.error(err.message);
+			process.exit(1);
+		});
 }
 
 function clearLine () {
@@ -103,42 +105,44 @@ function clearLine () {
 }
 
 function epack ({file, opts}) {
-	import('chalk').then(({default: chalk}) => {
-		process.stdout.write('\t' + path.basename(file.basename, '.js') + '... ');
-		const result = spawn.sync('enact', opts, {
-			cwd: process.cwd(),
-			env: {
-				...process.env,
-				...env,
-				ILIB_CONTEXT: path.dirname(file.fullPath),
-				ENACT_ALIAS: JSON.stringify({UI_TEST_APP_ENTRY: file.fullPath}),
-				PUBLIC_URL: '/' + path.basename(file.fullPath, '.js')
-			},
-			encoding: 'utf8'
-		});
-		if (result.status === 0) {
-			if (process.stdout.isTTY) {
-				clearLine();
-				process.stdout.write(chalk.green('\t✔ ') + path.basename(file.basename, '.js') + '\n');
-			} else {
-				process.stdout.write('DONE\n');
-			}
-		} else {
-			let err = '';
-			if (result.stdout) {
-				err += result.stdout.split(/\n+/).slice(2).join('\n');
-			}
-			if (result.stderr) err += '\n' + result.stderr;
-
-			if (process.stdout.isTTY) {
-				clearLine();
-				process.stdout.write(chalk.red('\t✖ ') + path.basename(file.basename, '.js') + '\n\n');
-			} else {
-				process.stdout.write('ERROR\n\n');
-			}
-			throw new Error(err || 'Unknown error');
-		}
+	process.stdout.write('\t' + path.basename(file.basename, '.js') + '... ');
+	const result = spawn.sync('enact', opts, {
+		cwd: process.cwd(),
+		env: {
+			...process.env,
+			...env,
+			ILIB_CONTEXT: path.dirname(file.fullPath),
+			ENACT_ALIAS: JSON.stringify({UI_TEST_APP_ENTRY: file.fullPath}),
+			PUBLIC_URL: '/' + path.basename(file.fullPath, '.js')
+		},
+		encoding: 'utf8'
 	});
+	if (result.status === 0) {
+		if (process.stdout.isTTY) {
+			clearLine();
+			import('chalk').then(({default: chalk}) => {
+				process.stdout.write(chalk.green('\t✔ ') + path.basename(file.basename, '.js') + '\n');
+			});
+		} else {
+			process.stdout.write('DONE\n');
+		}
+	} else {
+		let err = '';
+		if (result.stdout) {
+			err += result.stdout.split(/\n+/).slice(2).join('\n');
+		}
+		if (result.stderr) err += '\n' + result.stderr;
+
+		if (process.stdout.isTTY) {
+			clearLine();
+			import('chalk').then(({default: chalk}) => {
+				process.stdout.write(chalk.red('\t✖ ') + path.basename(file.basename, '.js') + '\n\n');
+			});
+		} else {
+			process.stdout.write('ERROR\n\n');
+		}
+		throw new Error(err || 'Unknown error');
+	}
 }
 
 module.exports = buildApps;
