8ca49a10d3c93c1687b9b3e7e7cbecdd06e94233
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2023-10-13 11:10:07 +0300

    set chrome driver path only if the process.env.CHROME_DRIVER_PATH value exists

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -109,9 +109,9 @@ module.exports.configure = (options) => {
 				'goog:chromeOptions': visibleBrowser ? {} : {
 					args: ['--headless', '--window-size=1920,1080']
 				},
-				'wdio:chromedriverOptions': {
+				'wdio:chromedriverOptions': process.env.CHROME_DRIVER_PATH ? {
 					binary: process.env.CHROME_DRIVER_PATH
-				}
+				} : {}
 			}],
 			//
 			// ===================
