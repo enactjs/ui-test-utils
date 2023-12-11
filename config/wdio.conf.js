ce4f2e69a8bceed2f602c9d694ff060e467d502c
dongsu.won <dongsu.won@lgepartner.com>
2023-12-11 09:50:33 +0900

    change

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -117,7 +117,7 @@ module.exports.configure = (options) => {
 			// Define all options that are relevant for the WebdriverIO instance here
 			//
 			// Level of logging verbosity: silent | verbose | command | data | result | error
-			logLevel: 'verbose',
+			logLevel: 'error',
 			//
 			// Enables colors for log output.
 			coloredLogs: true,
