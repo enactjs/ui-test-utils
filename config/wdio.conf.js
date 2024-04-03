921e75bb86a626dc77fd20359ea83e935f8686ed
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2024-04-03 17:01:34 +0300

    configured to run tests on TV

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -1,5 +1,5 @@
 const parseArgs = require('minimist');
-const {execSync} = require('child_process');
+const {readChromeVersion} = require('../utils/readChromeVersionNumber');
 
 const args = parseArgs(process.argv);
 
@@ -10,44 +10,11 @@ const visibleBrowser = !!args.visible,
 module.exports.configure = (options) => {
 	const {base, services} = options;
 	const opts = Object.assign({}, options);
+	const chromeVersionMajorNumber = readChromeVersion();
 
 	delete opts.base;
 	delete opts.before;
 	delete opts.services;
-
-	let chromeVersionMajorNumber;
-	if (!process.env.CHROME_DRIVER) {
-		if (process.env.TV_IP && process.argv.find(arg => arg.includes('tv.conf'))) {
-			process.env.CHROME_DRIVER = 2.44; // Currently, TV supports 83 and lower, but keep the previous version for safety.
-		} else {
-			try {
-				if (process.platform === 'win32') {
-					// Windows
-					const chromeVersion = /\d+/.exec(execSync('wmic datafile where "name=\'C:\\\\Program Files\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe\'" get Version /value').toString());
-					chromeVersionMajorNumber = (chromeVersion && chromeVersion[0]);
-				} else if (process.platform === 'darwin') {
-					// Mac
-					const chromeVersion = /Chrome (\d+)/.exec(execSync('/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --version'));
-					chromeVersionMajorNumber = (chromeVersion && chromeVersion[1]);
-				} else {
-					const chromeVersion = /Chrome (\d+)/.exec(execSync('google-chrome -version'));
-					chromeVersionMajorNumber = (chromeVersion && chromeVersion[1]);
-				}
-				const chromeDriverVersion = execSync('curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE' + (chromeVersionMajorNumber ? ('_' + chromeVersionMajorNumber) : ''));
-
-				if (chromeDriverVersion.includes('Error') || !/\d+.\d+.\d+.\d+/.exec(chromeDriverVersion)) {
-					throw new Error();
-				} else {
-					process.env.CHROME_DRIVER = chromeDriverVersion;
-				}
-			} catch (error) {
-				console.log('ERROR: Cannnot find Chrome driver from Chrome ' + chromeVersionMajorNumber);
-				process.env.CHROME_DRIVER = 2.44;
-			}
-		}
-
-		console.log('Chrome Driver Version : ' + process.env.CHROME_DRIVER);
-	}
 
 	return Object.assign(
 		opts,
--- ui/wdio.tv.conf.js
+++ ui/wdio.tv.conf.js
@@ -1,5 +1,8 @@
 const ipAddress = require('../utils/ipAddress.js');
 const {config} = require('./wdio.conf.js');
+const {readChromeVersion} = require('../utils/readChromeVersionNumber');
+
+const chromeVersionMajorNumber = readChromeVersion();
 
 exports.config = Object.assign(
 	{},
@@ -9,14 +12,19 @@ exports.config = Object.assign(
 			maxInstances: 1,
 
 			browserName: 'chrome',
-			browserVersion: 'stable',
 			'goog:chromeOptions': {
 				w3c: false,
-				debuggerAddress: `${process.env.TV_IP}:9998`
+				debuggerAddress: '192.168.0.45:9998'
+			},
+			'wdio:chromedriverOptions': process.env.CHROME_DRIVER_PATH ? {
+				binary: process.env.CHROME_DRIVER_PATH
+				// match chromedriver version from jenkins
+			} : Number(chromeVersionMajorNumber) > 108 ? {} : {
+				binary: 'C:\\chromedriver\\chromedriver_v108.exe'
 			}
 		}],
 
-		baseUrl: `http://${ipAddress()}:4567`,
+		baseUrl: 'http://192.168.0.38:4567',
 
 		before: function () {
 			if (config.before) {
