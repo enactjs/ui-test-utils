4635cca73701abf7d77592294b76370680bf2c77
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2024-04-24 16:00:25 +0300

    code cleanup and updated wdio to latest version

--- ui/wdio.tv.conf.js
+++ ui/wdio.tv.conf.js
@@ -1,8 +1,5 @@
 const ipAddress = require('../utils/ipAddress.js');
 const {config} = require('./wdio.conf.js');
-const {readChromeVersion} = require('../utils/readChromeVersionNumber');
-
-const chromeVersionMajorNumber = readChromeVersion();
 
 exports.config = Object.assign(
 	{},
@@ -14,17 +11,14 @@ exports.config = Object.assign(
 			browserName: 'chrome',
 			'goog:chromeOptions': {
 				w3c: false,
-				debuggerAddress: '192.168.0.45:9998'
+				debuggerAddress: `${process.env.TV_IP}:9998`
 			},
-			'wdio:chromedriverOptions': process.env.CHROME_DRIVER_PATH ? {
-				binary: process.env.CHROME_DRIVER_PATH
-				// match chromedriver version from jenkins
-			} : Number(chromeVersionMajorNumber) > 108 ? {} : {
+			'wdio:chromedriverOptions': {
 				binary: 'C:\\chromedriver\\chromedriver_v108.exe'
 			}
 		}],
 
-		baseUrl: 'http://192.168.0.38:4567',
+		baseUrl: `http://${ipAddress()}:4567`,
 
 		before: function () {
 			if (config.before) {
