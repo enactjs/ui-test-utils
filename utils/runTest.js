61747b3c3edcc2aae568356f958fe84ef8e355d0
Ion Andrusciac <ion.andrusciac@lgepartner.com>
2025-01-08 16:37:19 +0200

    Removed pause before taking screenshot

--- utils/runTest.js
+++ utils/runTest.js
@@ -63,8 +63,6 @@ const runTest = ({concurrency, filter, Page, testName, ...rest}) => {
 
 								await Page.open(`?${params}`);
 
-								await browser.pause(100);
-
 								expect(await browser.checkScreen(screenshotFileName, {
 									disableCSSAnimation: true,
 									ignoreNothing: true,
