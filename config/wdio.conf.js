cbcc7d418b070bd6d0c4a6f0232a39c91baf26ed
daniel-stoian-lgp <daniel.stoian@lgepartner.com>
2025-10-07 23:13:01 +0300

    set chrome 120 for screenshot tests

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -86,7 +86,7 @@ export const configure = (options) => {
 				/* WebdriverIO v8.14 and above downloads and uses the latest Chrome version when running tests.
 				We need to specify a browser version that matches chromedriver version running in CI/CD environment to
 				ensure testing accuracy. */
-				// browserVersion: process.env.CHROME_DRIVER,
+				browserVersion: process.env.CHROME_DRIVER,
 				'goog:chromeOptions': visibleBrowser ?
 					{
 						args: [
